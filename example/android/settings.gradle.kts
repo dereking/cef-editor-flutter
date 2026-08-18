5c301e974577c31910c6ba4cf415d1765fe9f4aa
dereking <ke_dong@126.com>
2026-08-19 00:56:40 +0800

    fix: MSB8064 增量依赖警告 + iOS ATS 文档

--- example/android/settings.gradle.kts
+++ example/android/settings.gradle.kts
@@ -19,8 +19,16 @@ pluginManagement {
 
 plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
-    id("com.android.application") version "9.0.1" apply false
-    id("org.jetbrains.kotlin.android") version "2.3.20" apply false
+    id("com.android.application") version "8.9.2" apply false
+    id("org.jetbrains.kotlin.android") version "2.1.10" apply false
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
+    repositories {
+        google()
+        mavenCentral()
+    }
 }
 
 include(":app")
